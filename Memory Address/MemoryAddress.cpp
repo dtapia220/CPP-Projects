2123868aea58c653a6ff39081817b0539ec5e577
dtapia220 <79428988+dtapia220@users.noreply.github.com>
2023-07-14 16:08:09 -0500

    Memory Address
    Displays the memory address of the variable

---
+++ Memory Address/MemoryAddress.cpp
@@ -0,0 +1,21 @@
+/*
+ * MemoryAddress.cpp
+ *
+ *  Created on: Apr 13, 2020
+ *      Author: Diego Tapia
+ */
+
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main(){
+
+	int num = - 23;
+
+	cout << &num; // THe memory address of variable num
+
+
+
+	return 0;
+}
